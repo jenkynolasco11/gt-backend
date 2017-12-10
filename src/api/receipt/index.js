e9d0ba72d20aee9bf87d94914e8bdee61fa9aab2
Jenky Nolasco <jenky.nolasco@gmail.com>
2017-12-10 15:58:11 -0500

    Meh

--- src/api/receipt/index.js
+++ src/api/receipt/index.js
@@ -1,9 +1,23 @@
 import Router from 'koa-router'
 
-import { Receipt } from '../../models'
+import { Receipt, Ticket } from '../../models'
 // import { Ride, RideDetail, Bus, } from '../../models'
 
 const receiptRouter = new Router({ prefix : 'receipt' })
+
+receiptRouter.get('/:id/tickets', async ctx => { 
+  const { id } = ctx.params
+  try {
+    const receipt = await Receipt.findOne({ id })
+    const tickets = await Ticket.find({ receipt : receipt._id })
+
+    if(tickets) return ctx.body = { ok : true, data : { tickets }, message : ''}
+  } catch (e) {
+    console.log(e)
+  }
+
+  return ctx.body = null
+})
 
 receiptRouter.get('/:id', async ctx => {
   const { id } = ctx.params
